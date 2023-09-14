4fe285aa1f74a1ec19b8d7fcfc2143ea37600334
Ankush Pathak <ankush.pathak@canonical.com>
2023-09-14 15:38:43 -0400

    Bump version: 0.1.3 → 0.1.4

--- oval_xml_feed_merge/__init__.py
+++ oval_xml_feed_merge/__init__.py
@@ -1,2 +1,2 @@
 """Top-level package for OVAL XML Feed Merge."""
-__version__ = "0.1.3"
+__version__ = "0.1.4"
--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/canonical/oval-xml-feed-merge",
-    version="0.1.3",
+    version="0.1.4",
     zip_safe=False,
 )
